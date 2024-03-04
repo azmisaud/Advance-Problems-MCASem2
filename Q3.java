5400eaf028f6c029b75d373ef8399c842d88845d
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-03-04 22:09:28 +0530

    Solution of Question3

---
+++ Q3.java
@@ -0,0 +1,21 @@
+
+public class Q3 {
+    public static void main(String[] args) {
+        String str=null;
+        //Handling null pointer exception using try-catch.
+        try{
+            System.out.println("String length : " +str.length());
+        } catch (NullPointerException e){
+            System.err.println(e.getMessage());
+        }
+        //Handling Null Pointer Exception using Default Exception Handler.
+        try{
+            System.out.println("String length : " +str.length());
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        //Preventing unchecked exception
+        if(str!=null) System.out.println("String length : " +str.length());
+        else System.out.println("String is null, can't find it's length.");
+    }
+}
